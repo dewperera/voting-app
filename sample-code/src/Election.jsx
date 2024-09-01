8e528d3078d5ebe46df0ed43871e0ea088a67d7c
hirushaaaaaa <hirusha.fernando2002@gmail.com>
2024-09-01 23:56:17 +0530

    Update Election.jsx

--- sample-code/src/Election.jsx
+++ sample-code/src/Election.jsx
@@ -1,1 +1,14 @@
+// src/Election.jsx
+import React from 'react';
 
+function Election() {
+  return (
+    <div>
+      <h1>Election</h1>
+      <p>This is the Election page.</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
+
+export default Election;
